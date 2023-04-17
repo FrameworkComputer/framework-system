f8e3fa544f76b5e7b1bf55a47629519a0bc32b68
Daniel Schaefer <dhs@frame.work>
2023-04-17 15:47:06 +0800

    Remove dependency on redox-dmi
    It doesn't support stable rust.
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- framework_lib/src/uefi/mod.rs
+++ framework_lib/src/uefi/mod.rs
@@ -99,6 +99,51 @@ pub fn enable_page_break() {
     }
 }
 
+#[repr(packed)]
+pub struct Smbios {
+    pub anchor: [u8; 4],
+    pub checksum: u8,
+    pub length: u8,
+    pub major_version: u8,
+    pub minor_version: u8,
+    pub max_structure_size: u16,
+    pub revision: u8,
+    pub formatted: [u8; 5],
+    pub inter_anchor: [u8; 5],
+    pub inter_checksum: u8,
+    pub table_length: u16,
+    pub table_address: u32,
+    pub structure_count: u16,
+    pub bcd_revision: u8,
+}
+
+impl Smbios {
+    pub fn checksum_valid(&self) -> bool {
+        let mut sum: u8 = self.anchor.iter().sum::<u8>();
+        sum += self.checksum;
+        sum += self.length;
+        sum += self.major_version;
+        sum += self.minor_version;
+        sum += self.max_structure_size as u8;
+        sum += self.revision;
+        sum += self.formatted.iter().sum::<u8>();
+        sum == 0
+    }
+}
+
+pub struct Smbios3 {
+    pub anchor: [u8; 5],
+    pub checksum: u8,
+    pub length: u8,
+    pub major_version: u8,
+    pub minor_version: u8,
+    pub docrev: u8,
+    pub revision: u8,
+    _reserved: u8,
+    pub table_length: u32,
+    pub table_address: u64,
+}
+
 pub fn smbios_data() -> Option<Vec<u8>> {
     let st = unsafe { uefi_services::system_table().as_ref() };
     let config_tables = st.config_table();
@@ -106,18 +151,16 @@ pub fn smbios_data() -> Option<Vec<u8>> {
     for table in config_tables {
         let table_data = match table.guid {
             SMBIOS3_GUID => unsafe {
-                let smbios = &*(table.address as *const dmi::Smbios);
-                // TODO: Seems to be invalid. Is the calculation correct?
-                //smbios.is_valid();
+                let smbios = &*(table.address as *const Smbios3);
+                assert!(smbios.anchor == *b"_SM3_");
                 Some(slice::from_raw_parts(
                     smbios.table_address as *const u8,
                     smbios.table_length as usize,
                 ))
             },
             SMBIOS_GUID => unsafe {
-                let smbios = &*(table.address as *const dmi::Smbios);
-                // TODO: Seems to be invalid. Is the calculation correct?
-                //smbios.is_valid();
+                let smbios = &*(table.address as *const Smbios);
+                assert!(smbios.checksum_valid());
                 Some(slice::from_raw_parts(
                     smbios.table_address as *const u8,
                     smbios.table_length as usize,
