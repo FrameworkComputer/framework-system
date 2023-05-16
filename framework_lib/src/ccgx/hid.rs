498cc7912e14c4c062afe64348ed04701a147625
Daniel Schaefer <dhs@frame.work>
2023-05-16 17:24:19 +0800

    ccgx: Allow FW Update for HDMI Card
    Still has problem. Doesn't seem to work when card is booted in
    BackupFw(1). And probably needs longer sleep to wait for restart.
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- framework_lib/src/ccgx/hid.rs
+++ framework_lib/src/ccgx/hid.rs
@@ -241,7 +241,7 @@ pub fn find_device(api: &HidApi) -> Option<HidDevice> {
         let vid = dev_info.vendor_id();
         let pid = dev_info.product_id();
         let usage_page = dev_info.usage_page();
-        if vid == FRAMEWORK_VID && [DP_CARD_PID].contains(&pid) && usage_page == CCG_USAGE_PAGE {
+        if vid == FRAMEWORK_VID && [DP_CARD_PID, HDMI_CARD_PID].contains(&pid) && usage_page == CCG_USAGE_PAGE {
             return Some(dev_info.open_device(api).unwrap());
         }
     }
