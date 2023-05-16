56c657a389498d183378143498d54ba331dbad42
Daniel Schaefer <dhs@frame.work>
2023-05-16 14:50:40 +0800

    os_specific: Add note about sleeping in UEFI
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- framework_lib/src/os_specific.rs
+++ framework_lib/src/os_specific.rs
@@ -12,6 +12,8 @@ pub fn sleep(micros: u64) {
     }
     #[cfg(feature = "uefi")]
     {
+        // TODO: It's not recommended to use this for sleep more than 10ms
+        // Should use a one-shot timer event
         let st = unsafe { uefi_services::system_table().as_ref() };
         let bs = st.boot_services();
         bs.stall(micros as usize);
