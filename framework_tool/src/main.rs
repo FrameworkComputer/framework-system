9a4dbe16a5335038d3ae9a4a675fecf2aab2ab30
Daniel Schaefer <dhs@frame.work>
2023-05-19 16:38:27 +0800

    fixup! Embed DP/HDMI fw update for standalone app

--- framework_tool/src/main.rs
+++ framework_tool/src/main.rs
@@ -3,14 +3,24 @@ use framework_lib::commandline;
 fn main() {
     if let Some(binfile) = option_env!("FWK_DP_HDMI_BIN") {
         let bin = match binfile {
-            "dp-flash-008" => include_bytes!("../../framework_lib/embed_bins/dp-flash-008.bin"),
-            "dp-flash-100" => include_bytes!("../../framework_lib/embed_bins/dp-flash-100.bin"),
-            "dp-flash-101" => include_bytes!("../../framework_lib/embed_bins/dp-flash-101.bin"),
-            "hdmi-flash-006" => include_bytes!("../../framework_lib/embed_bins/hdmi-flash-006.bin"),
-            "hdmi-flash-105" => include_bytes!("../../framework_lib/embed_bins/hdmi-flash-105.bin"),
+            "dp-flash-008" => {
+                include_bytes!("../../framework_lib/embed_bins/dp-flash-008.bin").as_slice()
+            }
+            "dp-flash-100" => {
+                include_bytes!("../../framework_lib/embed_bins/dp-flash-100.bin").as_slice()
+            }
+            "dp-flash-101" => {
+                include_bytes!("../../framework_lib/embed_bins/dp-flash-101.bin").as_slice()
+            }
+            "hdmi-flash-006" => {
+                include_bytes!("../../framework_lib/embed_bins/hdmi-flash-006.bin").as_slice()
+            }
+            "hdmi-flash-105" => {
+                include_bytes!("../../framework_lib/embed_bins/hdmi-flash-105.bin").as_slice()
+            }
             _ => unreachable!(),
         };
-        framework_lib::ccgx::hid::flash_firmware(bin.as_slice());
+        framework_lib::ccgx::hid::flash_firmware(bin);
     } else {
         commandline::print_dp_hdmi_details();
     }
