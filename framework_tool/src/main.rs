d09ef935488ed242615940794964a59c7dd75646
Daniel Schaefer <dhs@frame.work>
2023-05-19 17:14:58 +0800

    fixup! dp/hdmi updater: Allow update only on compatible card

--- framework_tool/src/main.rs
+++ framework_tool/src/main.rs
@@ -3,21 +3,26 @@ use framework_lib::commandline;
 fn main() {
     if let Some(binfile) = option_env!("FWK_DP_HDMI_BIN") {
         let (bin, pid) = match binfile {
-            "dp-flash-008" => {
-                (include_bytes!("../../framework_lib/embed_bins/dp-flash-008.bin").as_slice(), framework_lib::ccgx::hid::DP_CARD_PID)
-            }
-            "dp-flash-100" => {
-                (include_bytes!("../../framework_lib/embed_bins/dp-flash-100.bin").as_slice(), framework_lib::ccgx::hid::DP_CARD_PID)
-            }
-            "dp-flash-101" => {
-                (include_bytes!("../../framework_lib/embed_bins/dp-flash-101.bin").as_slice(), framework_lib::ccgx::hid::DP_CARD_PID)
-            }
-            "hdmi-flash-006" => {
-                (include_bytes!("../../framework_lib/embed_bins/hdmi-flash-006.bin").as_slice(), framework_lib::ccgx::hid::HDMI_CARD_PID)
-            }
-            "hdmi-flash-105" => {
-                (include_bytes!("../../framework_lib/embed_bins/hdmi-flash-105.bin").as_slice(), framework_lib::ccgx::hid::HDMI_CARD_PID)
-            }
+            "dp-flash-008" => (
+                include_bytes!("../../framework_lib/embed_bins/dp-flash-008.bin").as_slice(),
+                framework_lib::ccgx::hid::DP_CARD_PID,
+            ),
+            "dp-flash-100" => (
+                include_bytes!("../../framework_lib/embed_bins/dp-flash-100.bin").as_slice(),
+                framework_lib::ccgx::hid::DP_CARD_PID,
+            ),
+            "dp-flash-101" => (
+                include_bytes!("../../framework_lib/embed_bins/dp-flash-101.bin").as_slice(),
+                framework_lib::ccgx::hid::DP_CARD_PID,
+            ),
+            "hdmi-flash-006" => (
+                include_bytes!("../../framework_lib/embed_bins/hdmi-flash-006.bin").as_slice(),
+                framework_lib::ccgx::hid::HDMI_CARD_PID,
+            ),
+            "hdmi-flash-105" => (
+                include_bytes!("../../framework_lib/embed_bins/hdmi-flash-105.bin").as_slice(),
+                framework_lib::ccgx::hid::HDMI_CARD_PID,
+            ),
             _ => unreachable!(),
         };
         framework_lib::ccgx::hid::flash_firmware(bin, &[pid]);
