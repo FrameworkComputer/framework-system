FrameworkComputer/framework-system

commit d2b33f9cce2709fb5056d38253434fbdd8a55d22

Replace redox_uefi_std with uefi-rs
Much more mature, well maintained crate.

Signed-off-by: Daniel Schaefer <dhs@frame.work>

framework_lib/src/chromium_ec/mod.rs
@@ -23,6 +23,11 @@ mod portio;
 #[cfg(feature = "win_driver")]
 mod windows;
 
+use alloc::format;
+use alloc::string::String;
+use alloc::string::ToString;
+use alloc::vec;
+use alloc::vec::Vec;
 #[cfg(feature = "uefi")]
 use core::prelude::rust_2021::derive;
 
@@ -284,6 +289,8 @@ impl CrosEc {
                 }
             };
             cmd.subcmd = ConsoleReadSubCommand::ConsoleReadNext as u8;
+
+            // Need to explicitly handle CTRL-C termination on UEFI Shell
             #[cfg(feature = "uefi")]
             if shell_get_execution_break_flag() {
                 return Ok(console);

framework_lib/src/commandline/mod.rs
@@ -3,6 +3,10 @@
 //! Can be easily re-used from any OS or UEFI shell.
 //! We have implemented both in the `framework_tool` and `framework_uefi` crates.
 
+use alloc::string::String;
+use alloc::string::ToString;
+use alloc::vec::Vec;
+
 #[cfg(not(feature = "uefi"))]
 pub mod clap_std;
 #[cfg(feature = "uefi")]
@@ -363,7 +367,7 @@ pub fn run_with_args(args: &Cli, _allupdate: bool) -> i32 {
     //    raw_command(&args[1..]);
     } else if let Some(pd_bin_path) = &args.pd_bin {
         #[cfg(feature = "uefi")]
-        let data = crate::uefi::fs::shell_read_file(pd_bin_path);
+        let data: Option<Vec<u8>> = crate::uefi::fs::shell_read_file(pd_bin_path);
         #[cfg(not(feature = "uefi"))]
         let data = match fs::read(pd_bin_path) {
             Ok(data) => Some(data),
@@ -382,7 +386,7 @@ pub fn run_with_args(args: &Cli, _allupdate: bool) -> i32 {
         }
     } else if let Some(ec_bin_path) = &args.ec_bin {
         #[cfg(feature = "uefi")]
-        let data = crate::uefi::fs::shell_read_file(ec_bin_path);
+        let data: Option<Vec<u8>> = crate::uefi::fs::shell_read_file(ec_bin_path);
         #[cfg(not(feature = "uefi"))]
         let data = match fs::read(ec_bin_path) {
             Ok(data) => Some(data),
@@ -401,7 +405,7 @@ pub fn run_with_args(args: &Cli, _allupdate: bool) -> i32 {
         }
     } else if let Some(capsule_path) = &args.capsule {
         #[cfg(feature = "uefi")]
-        let data = crate::uefi::fs::shell_read_file(capsule_path);
+        let data: Option<Vec<u8>> = crate::uefi::fs::shell_read_file(capsule_path);
         #[cfg(not(feature = "uefi"))]
         let data = match fs::read(capsule_path) {
             Ok(data) => Some(data),
@@ -416,9 +420,8 @@ pub fn run_with_args(args: &Cli, _allupdate: bool) -> i32 {
             println!("File");
             println!("  Size:       {:>20} B", data.len());
             println!("  Size:       {:>20} KB", data.len() / 1024);
-            if let Some(_header) = analyze_capsule(&data) {
-                // TODO: For now we can only read files on UEFI, not write them
-                if _header.capsule_guid == esrt::WINUX_GUID {
+            if let Some(header) = analyze_capsule(&data) {
+                if header.capsule_guid == esrt::WINUX_GUID {
                     let ux_header = capsule::parse_ux_header(&data);
                     if let Some(dump_path) = &args.dump {
                         // TODO: Better error handling, rather than just panicking

framework_lib/src/esrt/mod.rs
@@ -10,12 +10,19 @@
 //!
 //! Not all firmware components are reported here.
 
-use core::fmt;
+#[allow(unused_imports)]
+use log::{debug, error, info, trace};
+use std::prelude::v1::*;
+
+#[cfg(not(feature = "uefi"))]
+use crate::guid::Guid;
 use core::prelude::v1::derive;
+#[cfg(not(feature = "uefi"))]
+use guid_macros::guid;
 #[cfg(feature = "uefi")]
 use std::slice;
 #[cfg(feature = "uefi")]
-use std::uefi::guid::GuidKind;
+use uefi::{guid, Guid};
 
 #[cfg(feature = "linux")]
 use std::fs;
@@ -24,30 +31,14 @@ use std::io;
 #[cfg(feature = "linux")]
 use std::path::Path;
 
-#[derive(Copy, Clone, Debug, Eq, PartialEq)]
-#[repr(C)]
-pub struct Guid(pub u32, pub u16, pub u16, pub [u8; 8]);
-impl fmt::Display for Guid {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({:>08x}, {:>04x}, {:>04x}, [", self.0, self.1, self.2)?;
-        for (i, b) in self.3.iter().enumerate() {
-            if i > 0 {
-                write!(f, ",")?;
-            }
-            write!(f, "{:>02x}", b)?;
-        }
-        write!(f, "])")?;
-        Ok(())
-    }
-}
-
 /// Decode from GUID string version
 ///
 /// # Examples
 /// ```
 /// use framework_lib::esrt::*;
+/// use framework_lib::guid::*;
 ///
-/// let valid_guid = Guid(0xA9C91B0C, 0xC0B8, 0x463D, [0xA7, 0xDA, 0xA5, 0xD6, 0xEC, 0x64, 0x63, 0x33]);
+/// let valid_guid = Guid::from_values(0xA9C91B0C, 0xC0B8, 0x463D, 0xA7DA, 0xA5D6EC646333);
 /// // Works with lower-case
 /// let guid = guid_from_str("a9c91b0c-c0b8-463d-a7da-a5d6ec646333");
 /// assert_eq!(guid, Some(valid_guid));
@@ -60,53 +51,28 @@ impl fmt::Display for Guid {
 /// ```
 pub fn guid_from_str(string: &str) -> Option<Guid> {
     let sections: Vec<&str> = string.split('-').collect();
-    let first = u32::from_str_radix(sections[0], 16).ok()?;
-    let second = u16::from_str_radix(sections[1], 16).ok()?;
-    let third = u16::from_str_radix(sections[2], 16).ok()?;
+    let time_low = u32::from_str_radix(sections[0], 16).ok()?;
+    let time_mid = u16::from_str_radix(sections[1], 16).ok()?;
+    let time_high_and_version = u16::from_str_radix(sections[2], 16).ok()?;
+    let clock_seq_and_variant = u16::from_str_radix(sections[3], 16).ok()?;
+    let node = u64::from_str_radix(sections[4], 16).ok()?;
 
-    let fourth = {
-        [
-            u8::from_str_radix(&sections[3][0..2], 16).ok()?,
-            u8::from_str_radix(&sections[3][2..4], 16).ok()?,
-            u8::from_str_radix(&sections[4][0..2], 16).ok()?,
-            u8::from_str_radix(&sections[4][2..4], 16).ok()?,
-            u8::from_str_radix(&sections[4][4..6], 16).ok()?,
-            u8::from_str_radix(&sections[4][6..8], 16).ok()?,
-            u8::from_str_radix(&sections[4][8..10], 16).ok()?,
-            u8::from_str_radix(&sections[4][10..12], 16).ok()?,
-        ]
-    };
-
-    Some(Guid(first, second, third, fourth))
+    Some(Guid::from_values(
+        time_low,
+        time_mid,
+        time_high_and_version,
+        clock_seq_and_variant,
+        node,
+    ))
 }
 
-pub const BIOS_GUID: Guid = Guid(
-    0xa30a8cf3,
-    0x847f,
-    0x5e59,
-    [0xbd, 0x59, 0xf9, 0xec, 0x14, 0x5c, 0x1a, 0x8c],
-);
-pub const RETIMER01_GUID: Guid = Guid(
-    0xa9c91b0c,
-    0xc0b8,
-    0x463d,
-    [0xa7, 0xda, 0xa5, 0xd6, 0xec, 0x64, 0x63, 0x33],
-);
-pub const RETIMER23_GUID: Guid = Guid(
-    0xba2e4e6e,
-    0x3b0c,
-    0x4f25,
-    [0x8a, 0x59, 0x4c, 0x55, 0x3f, 0xc8, 0x6e, 0xa2],
-);
+pub const BIOS_GUID: Guid = guid!("a30a8cf3-847f-5e59-bd59-f9ec145c1a8c");
+pub const RETIMER01_GUID: Guid = guid!("a9c91b0c-c0b8-463d-a7da-a5d6ec646333");
+pub const RETIMER23_GUID: Guid = guid!("ba2e4e6e-3b0c-4f25-8a59-4c553fc86ea2");
 // In EDK2
 // Handled by MdeModulePkg/Library/DxeCapsuleLibFmp/DxeCapsuleLib.c
 // Defined by MdePkg/Include/IndustryStandard/WindowsUxCapsule.h
-pub const WINUX_GUID: Guid = Guid(
-    0x3b8c8162,
-    0x188c,
-    0x46a4,
-    [0xae, 0xc9, 0xbe, 0x43, 0xf1, 0xd6, 0x56, 0x97],
-);
+pub const WINUX_GUID: Guid = guid!("3b8c8162-188c-46a4-aec9-be43f1d65697");
 
 #[derive(Debug)]
 pub enum FrameworkGuidKind {
@@ -216,7 +182,7 @@ pub fn print_esrt(esrt: &Esrt) {
 
     for (i, entry) in esrt.entries.iter().enumerate() {
         println!("ESRT Entry {}", i);
-        println!("  GUID:                 {}", entry.fw_class);
+        println!("  GUID:                 {:?}", entry.fw_class);
         println!(
             "  GUID:                 {:?}",
             match_guid_kind(&entry.fw_class)
@@ -329,12 +295,20 @@ pub fn get_esrt() -> Option<Esrt> {
     None
 }
 
+pub const SYSTEM_RESOURCE_TABLE_GUID: Guid = guid!("b122a263-3661-4f68-9929-78f8b0d62180");
+
 #[cfg(feature = "uefi")]
 pub fn get_esrt() -> Option<Esrt> {
-    for table in std::system_table().config_tables() {
-        match table.VendorGuid.kind() {
-            GuidKind::SystemResource => unsafe {
-                let raw_esrt = &*(table.VendorTable as *const _Esrt);
+    let st = unsafe { uefi_services::system_table().as_ref() };
+    let config_tables = st.config_table();
+
+    for table in config_tables {
+        // TODO: Why aren't they the same type?
+        //debug!("Table: {:?}", table);
+        let table_guid: Guid = unsafe { std::mem::transmute(table.guid) };
+        match table_guid {
+            SYSTEM_RESOURCE_TABLE_GUID => unsafe {
+                let raw_esrt = &*(table.address as *const _Esrt);
                 let mut esrt = Esrt {
                     resource_count: raw_esrt.resource_count,
                     resource_count_max: raw_esrt.resource_count_max,

framework_lib/src/uefi/mod.rs
@@ -1,29 +1,112 @@
+use alloc::vec::Vec;
 use core::slice;
-use std::proto::Protocol;
-use std::uefi::guid::GuidKind;
+use uefi::table::boot::{OpenProtocolAttributes, OpenProtocolParams, ScopedProtocol, SearchType};
+
+#[allow(unused_imports)]
+use log::{debug, error, info, trace};
+use uefi::proto::shell::Shell;
+use uefi::table::cfg::{SMBIOS3_GUID, SMBIOS_GUID};
+use uefi::table::{Boot, SystemTable};
+use uefi::Identify;
 
 pub mod fs;
 
+pub fn get_system_table() -> &'static SystemTable<Boot> {
+    unsafe { uefi_services::system_table().as_ref() }
+}
+
+fn find_shell_handle() -> Option<ScopedProtocol<'static, Shell>> {
+    let st = unsafe { uefi_services::system_table().as_ref() };
+    let boot_services = st.boot_services();
+    let shell_handles = boot_services.locate_handle_buffer(SearchType::ByProtocol(&Shell::GUID));
+    if let Ok(sh_buf) = shell_handles {
+        for handle in &*sh_buf {
+            return Some(unsafe {
+                boot_services
+                    .open_protocol::<Shell>(
+                        OpenProtocolParams {
+                            handle: *handle,
+                            agent: boot_services.image_handle(),
+                            controller: None,
+                        },
+                        OpenProtocolAttributes::GetProtocol,
+                    )
+                    .expect("Failed to open Shell handle")
+            });
+        }
+    } else {
+        panic!("No shell handle found!");
+    }
+    None
+}
+
 /// Returns true when the execution break was requested, false otherwise
 pub fn shell_get_execution_break_flag() -> bool {
-    let shell = if let Ok(shell) = fs::Shell::locate_protocol() {
-        shell
-    } else {
-        println!("Failed to open Shell Protocol");
-        return false;
-    };
+    let st = unsafe { uefi_services::system_table().as_ref() };
+    let boot_services = st.boot_services();
+    let shell_handles = boot_services.locate_handle_buffer(SearchType::ByProtocol(&Shell::GUID));
+    if let Ok(sh_buf) = shell_handles {
+        for handle in &*sh_buf {
+            let shell_handle = unsafe {
+                boot_services
+                    .open_protocol::<Shell>(
+                        OpenProtocolParams {
+                            handle: *handle,
+                            agent: boot_services.image_handle(),
+                            controller: None,
+                        },
+                        OpenProtocolAttributes::GetProtocol,
+                    )
+                    .expect("Failed to open Shell handle")
+            };
 
-    let uefi = std::system_table();
-    let check_event = uefi.BootServices.CheckEvent;
-    let status = (check_event)(shell.0.ExecutionBreak);
-    status.0 == 0
+            let event = unsafe { shell_handle.execution_break.unsafe_clone() };
+            return boot_services.check_event(event).unwrap();
+        }
+        return false;
+    } else {
+        panic!("No shell handle found!");
+    }
+}
+
+/// Enable pagination in UEFI shell
+///
+/// Pagination is handled by the UEFI shell environment automatically, whenever
+/// the application prints more than fits on the screen.
+pub fn enable_page_break() {
+    let st = unsafe { uefi_services::system_table().as_ref() };
+    let boot_services = st.boot_services();
+    let shell_handles = boot_services.locate_handle_buffer(SearchType::ByProtocol(&Shell::GUID));
+    if let Ok(sh_buf) = shell_handles {
+        for handle in &*sh_buf {
+            //trace!("Calling enable_page_break");
+            let shell_handle = unsafe {
+                boot_services
+                    .open_protocol::<Shell>(
+                        OpenProtocolParams {
+                            handle: *handle,
+                            agent: boot_services.image_handle(),
+                            controller: None,
+                        },
+                        OpenProtocolAttributes::GetProtocol,
+                    )
+                    .expect("Failed to open Shell handle")
+            };
+            shell_handle.enable_page_break();
+        }
+    } else {
+        panic!("No shell handle found!");
+    }
 }
 
 pub fn smbios_data() -> Option<Vec<u8>> {
-    for config_table in std::system_table().config_tables() {
-        let table_data = match config_table.VendorGuid.kind() {
-            GuidKind::Smbios => unsafe {
-                let smbios = &*(config_table.VendorTable as *const dmi::Smbios);
+    let st = unsafe { uefi_services::system_table().as_ref() };
+    let config_tables = st.config_table();
+
+    for table in config_tables {
+        let table_data = match table.guid {
+            SMBIOS3_GUID => unsafe {
+                let smbios = &*(table.address as *const dmi::Smbios);
                 // TODO: Seems to be invalid. Is the calculation correct?
                 //smbios.is_valid();
                 Some(slice::from_raw_parts(
@@ -31,10 +114,10 @@ pub fn smbios_data() -> Option<Vec<u8>> {
                     smbios.table_length as usize,
                 ))
             },
-            GuidKind::Smbios3 => unsafe {
+            SMBIOS_GUID => unsafe {
+                let smbios = &*(table.address as *const dmi::Smbios);
                 // TODO: Seems to be invalid. Is the calculation correct?
                 //smbios.is_valid();
-                let smbios = &*(config_table.VendorTable as *const dmi::Smbios3);
                 Some(slice::from_raw_parts(
                     smbios.table_address as *const u8,
                     smbios.table_length as usize,

.github/workflows/ci.yml
@@ -18,8 +18,6 @@ jobs:
 #     with:
 #       ssh-private-key: |
 #           ${{ secrets.PRIVKEY_SMBIOS_LIB }}
-#           ${{ secrets.PRIVKEY_REDOX_UEFI_STD }}
-
 #   - name: Install dependencies
 #     run: |
 #       sudo apt-get update
@@ -52,7 +50,6 @@ jobs:
       with:
         ssh-private-key: |
             ${{ secrets.PRIVKEY_SMBIOS_LIB }}
-            ${{ secrets.PRIVKEY_REDOX_UEFI_STD }}
 
     - name: Install dependencies
       run: |
@@ -104,7 +101,6 @@ jobs:
       with:
         ssh-private-key: |
             ${{ secrets.PRIVKEY_SMBIOS_LIB }}
-            ${{ secrets.PRIVKEY_REDOX_UEFI_STD }}
 
     - name: Setup Rust toolchain
       run: rustup show
@@ -138,7 +134,6 @@ jobs:
         with:
           ssh-private-key: |
             ${{ secrets.PRIVKEY_SMBIOS_LIB }}
-            ${{ secrets.PRIVKEY_REDOX_UEFI_STD }}
 
       - name: Install dependencies
         run: |
@@ -163,7 +158,6 @@ jobs:
       with:
         ssh-private-key: |
             ${{ secrets.PRIVKEY_SMBIOS_LIB }}
-            ${{ secrets.PRIVKEY_REDOX_UEFI_STD }}
 
     - name: Install dependencies
       run: |
@@ -194,7 +188,6 @@ jobs:
       with:
         ssh-private-key: |
             ${{ secrets.PRIVKEY_SMBIOS_LIB }}
-            ${{ secrets.PRIVKEY_REDOX_UEFI_STD }}
 
     - name: Install dependencies
       run: |

Cargo.toml
@@ -18,5 +18,9 @@ default-members = [
     "framework_tool",
 ]
 
+[patch.crates-io]
+uefi = { git = "https://github.com/FrameworkComputer/uefi-rs", branch = "merged" }
+uefi-services = { git = "https://github.com/FrameworkComputer/uefi-rs", branch = "merged" }
+
 [profile.release]
 lto = true

framework_lib/Cargo.toml
@@ -12,7 +12,7 @@ linux = ["linux_pio", "unix"]
 windows = ["std", "smbios", "dep:windows", "win_driver"]
 smbios = ["dep:smbios-lib"]
 std = ["dep:regex", "dep:clap", "smbios-lib?/std", "dep:hidapi", "dep:rusb"]
-uefi = ["dep:redox_uefi_std", "dep:redox_dmi", "dep:plain", "raw_pio", "smbios", "lazy_static/spin_no_std"]
+uefi = ["dep:redox_dmi", "dep:plain", "raw_pio", "smbios", "lazy_static/spin_no_std", "dep:uefi", "dep:uefi-services"]
 
 # EC communication via Port I/O on Linux
 linux_pio = ["dep:libc"]
@@ -34,17 +34,16 @@ nix = { version = "0.25.0", optional = true }
 num = { version = "0.4", default-features = false }
 num-derive = { version = "0.3", default-features = false }
 num-traits = { version = "0.2", default-features = false }
+log = "0.4"
+uefi = { version = "0.20", features = ["alloc"], optional = true }
+uefi-services = { version = "0.17", optional = true }
 redox_dmi = { version = "0.1.5", optional = true }
 plain = { version = "0.2.3", optional = true }
 spin = { version = "0.9.4", optional = false }
 hidapi = { version = "2.1.0", optional = true }
 rusb = { version = "0.9.1", optional = true }
-
-[dependencies.redox_uefi_std]
-optional = true
-git = "ssh://git@github.com/FrameworkComputer/redox_uefi_std.git"
-branch = "check-event"
-#path = "../../uefi/crates/uefi_std"
+no-std-compat = { version = "0.4.1", features = [ "alloc" ] }
+guid_macros = { path = "../guid_macros" }
 
 [dependencies.smbios-lib]
 git = "ssh://git@github.com/FrameworkComputer/smbios-lib.git"

framework_lib/src/capsule.rs
@@ -8,13 +8,18 @@
 //!
 //! Currently NOT implemented is parsing capsules with mutiple header structs!
 
+use std::prelude::v1::*;
+
 use core::prelude::rust_2021::derive;
 #[cfg(all(not(feature = "uefi"), feature = "std"))]
 use std::fs::File;
 #[cfg(all(not(feature = "uefi"), feature = "std"))]
 use std::io::prelude::*;
 
-use crate::esrt::Guid;
+#[cfg(not(feature = "uefi"))]
+use crate::guid::Guid;
+#[cfg(feature = "uefi")]
+use uefi::Guid;
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 #[repr(C)]
@@ -183,8 +188,8 @@ pub fn dump_winux_image(data: &[u8], header: &DisplayCapsule, filename: &str) {
     #[cfg(feature = "uefi")]
     {
         let ret = crate::uefi::fs::shell_write_file(filename, image);
-        if ret.0 != 0 {
-            println!("Failed to dump winux image.");
+        if let Err(err) = ret {
+            println!("Failed to dump winux image: {:?}", err);
         }
     }
 }

commit 2b35aa94098a498b8f703271af9819acc2ed4c05

Fix reading EC console with new UEFI library
`--console` on UEFI shell has probably been broken since
d2b33f9cce2709fb5056d38253434fbdd8a55d22

Signed-off-by: Daniel Schaefer <dhs@frame.work>

framework_lib/src/chromium_ec/mod.rs
@@ -8,6 +8,7 @@
 //! - `portio` - It uses raw port I/O. This works on UEFI and on Linux if the system isn't in lockdown mode (SecureBoot disabled).
 //! - `windows` - It uses [DHowett's Windows driver](https://github.com/DHowett/FrameworkWindowsUtils)
 
+use crate::os_specific;
 use crate::smbios;
 #[cfg(feature = "uefi")]
 use crate::uefi::shell_get_execution_break_flag;
@@ -287,14 +288,21 @@ impl CrosEc {
         loop {
             match cmd.send_command_vec(self) {
                 Ok(data) => {
-                    // I think that means the buffer is empty
+                    // EC Buffer is empty. We can wait a bit and see if there's more
+                    // Can't run it too quickly, otherwise the commands might fail
                     if data.is_empty() {
-                        return Ok(console);
+                        trace!("Empty EC response");
+                        println!("---");
+                        os_specific::sleep(1_000_000); // 1s
                     }
 
-                    let string = std::str::from_utf8(&data).unwrap();
-                    print!("{}", string);
-                    console.push_str(string);
+                    let utf8 = std::str::from_utf8(&data).unwrap();
+                    let ascii = utf8
+                        .replace(|c: char| !c.is_ascii(), "")
+                        .replace(|c: char| c == '\0', "");
+
+                    print!("{}", ascii);
+                    console.push_str(ascii.as_str());
                 }
                 Err(err) => {
                     println!("Err: {:?}", err);
@@ -318,7 +326,11 @@ impl CrosEc {
             subcmd: ConsoleReadSubCommand::ConsoleReadRecent as u8,
         }
         .send_command_vec(self)?;
-        Ok(std::str::from_utf8(&data).unwrap().to_string())
+        let utf8 = std::str::from_utf8(&data).unwrap();
+        let ascii = utf8
+            .replace(|c: char| !c.is_ascii(), "")
+            .replace(|c: char| c == '\0', "");
+        Ok(ascii)
     }
 }
 