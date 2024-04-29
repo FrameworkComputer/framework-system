6a820cb80a0f40adf627debd16d7a2e0e03bcd8f
Daniel Schaefer <dhs@frame.work>
2024-04-29 11:34:13 +0800

    Allow decoding of SMBIOS Type 11 serialnumbers
    They're the serialnumbers of what the system was originally assembled
    with in the factory.
    
    TODO
    
    - [ ] Cleanup code
    - [ ] Make safer with fewer unwraps
    - [ ] Custom command, not in info
    - [ ] Make sure date is decoded correctly
    - [ ] Support Framework 12
    - [x] Support Framework 13
    - [ ] Support Framework 16
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- framework_lib/src/commandline/uefi.rs
+++ framework_lib/src/commandline/uefi.rs
@@ -118,6 +118,7 @@ pub fn parse(args: &[String]) -> Cli {
         flash_gpu_descriptor: None,
         allupdate: false,
         info: false,
+        serialnums: false,
         raw_command: vec![],
     };
 
@@ -231,6 +232,9 @@ pub fn parse(args: &[String]) -> Cli {
             found_an_option = true;
         } else if arg == "--info" {
             cli.info = true;
+            found_an_option = true;
+        } else if arg == "--serialnums" {
+            cli.serialnums = true;
             found_an_option = true;
         } else if arg == "--intrusion" {
             cli.intrusion = true;
