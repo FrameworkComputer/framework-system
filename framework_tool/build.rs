f02ff876284a80bd22e28e9a0633d837c997c60a
Daniel Schaefer <dhs@frame.work>
2024-07-17 17:18:09 +0800

    windows: Add app icon
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- framework_tool/build.rs
+++ framework_tool/build.rs
@@ -1,3 +1,19 @@
-fn main() {
+use {
+    std::{
+        env,
+        io,
+    },
+    winresource::WindowsResource,
+};
+
+fn main() -> io::Result<()> {
+    if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        WindowsResource::new()
+            .set_icon("..\\res\\framework_startmenuicon.ico")
+            .compile()?;
+    }
+
     static_vcruntime::metabuild();
+
+    Ok(())
 }
