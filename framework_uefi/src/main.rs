226a57b01ddb084847fdd9bedcf11d6969c74643
Daniel Schaefer <dhs@frame.work>
2024-08-08 17:13:48 +0800

    WIP: Minimal uefi 0.30 build
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- framework_uefi/src/main.rs
+++ framework_uefi/src/main.rs
@@ -4,21 +4,19 @@
 #[allow(unused_imports)]
 use log::{debug, error, info, trace};
 use uefi::prelude::*;
-#[allow(unused_imports)]
-use uefi_services::{print, println};
 
-extern crate alloc;
+// extern crate alloc;
 
-use framework_lib::commandline;
+//use framework_lib::commandline;
 
 #[entry]
 fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
-    uefi_services::init(&mut system_table).unwrap();
+    uefi::helpers::init(&mut system_table).unwrap();
     let bs = system_table.boot_services();
 
-    let args = commandline::uefi::get_args(bs);
-    let args = commandline::parse(&args);
-    commandline::run_with_args(&args, false);
+    //let args = commandline::uefi::get_args(bs);
+    //let args = commandline::parse(&args);
+    //commandline::run_with_args(&args, false);
 
     // Force it go into UEFI shell
     Status::LOAD_ERROR
