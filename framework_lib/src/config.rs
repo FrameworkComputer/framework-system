7b787cd8ceaf42bcca796720f9798b4fb11dba38
Daniel Schaefer <dhs@frame.work>
2025-02-09 15:05:51 +0800

    Load platform settings from config
    This is useful instead of the commandline parameters
    --has-mec --pd-ports --pd-addrs
    when SMBIOS doesn't correctly detect the correct platform settings.
    
    - [x] Read from CWD
      - [x] Test on Windows
      - [x] Test on Linux
      - [ ] Test on UEFI
    - [x] Read from same folder as binary
      - [x] Test on Windows
      - [x] Test on Linux
      - [ ] Test on UEFI
    - [ ] Read from $XDG_CONFIG_HOME on Linux
    
    Sample config for Framework 13 AMD:
    
    ```
    [platform]
    has_mec = false
    pd_addrs = [ 0x42, 0x40 ]
    pd_ports = [ 1, 2 ]
    ```
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

---
+++ framework_lib/src/config.rs
@@ -0,0 +1,47 @@
+use serde::Deserialize;
+
+use crate::util;
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    platform: Option<Platform>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Platform {
+    has_mec: bool,
+    pd_addrs: Vec<u16>,
+    pd_ports: Vec<u8>,
+}
+
+const CONFIG_FILE: &str = "framework_tool_config.toml";
+
+pub fn load_config() -> Option<util::Platform> {
+    let mut path = std::env::current_exe().unwrap();
+    path.pop();
+    path.push(CONFIG_FILE);
+
+    let toml_str = if let Ok(str) = std::fs::read_to_string(path) {
+        str
+    } else {
+        path = CONFIG_FILE.into();
+        std::fs::read_to_string(path).unwrap()
+    };
+
+    let decoded: Config = toml::from_str(&toml_str).unwrap();
+    println!("{:?}", decoded);
+
+    let decoded = decoded.platform.unwrap();
+    let first_pd = (decoded.pd_addrs[0], decoded.pd_ports[0]);
+    let second_pd = if decoded.pd_addrs.is_empty() || decoded.pd_ports.is_empty() {
+        first_pd
+    } else {
+        (decoded.pd_addrs[1], decoded.pd_ports[1])
+    };
+
+    Some(util::Platform::GenericFramework(
+        (first_pd.0, second_pd.0),
+        (first_pd.1, second_pd.1),
+        decoded.has_mec,
+    ))
+}
