d4c0548ab2159214ec19e8585c0843ce246ce62d
Daniel Schaefer <dhs@frame.work>
2025-02-14 17:10:00 +0800

    power: No VCCGT temp sensor on gen11
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- framework_lib/src/power.rs
+++ framework_lib/src/power.rs
@@ -214,7 +214,12 @@ pub fn print_thermal(ec: &CrosEc) {
             println!("  F75303_DDR:   {:>4}", TempSensor::from(temps[2]));
             println!("  Battery:      {:>4}", TempSensor::from(temps[3]));
             println!("  PECI:         {:>4}", TempSensor::from(temps[4]));
-            println!("  F57397_VCCGT: {:>4}", TempSensor::from(temps[5]));
+            if matches!(
+                platform,
+                Some(Platform::IntelGen12) | Some(Platform::IntelGen13)
+            ) {
+                println!("  F57397_VCCGT: {:>4}", TempSensor::from(temps[5]));
+            }
         }
         Some(Platform::Framework13Amd7080 | Platform::Framework16Amd7080) => {
             println!("  F75303_Local: {:>4}", TempSensor::from(temps[0]));
