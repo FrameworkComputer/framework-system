865afe038d4c4c6fb18e2f445dfe6bfbd1d71347
Daniel Schaefer <dhs@frame.work>
2025-04-23 01:45:35 -0700

    inputmodule: Show location
    Otherwise if you have two LED matrices, you don't know which one's
    which.
    
    ```
    > framework_tool --versions
    [...]
    LED Matrix
      Firmware Version: 0.2.0
      Location: [X] [ ] [ ]       [ ] [ ]
    LED Matrix
      Firmware Version: 0.2.0
      Location: [ ] [ ] [ ]       [ ] [X]
    Laptop 16 ANSI Keyboard
      Firmware Version: 0.1.6
      Location: [ ] [X] [ ]       [ ] [ ]
    [...]
    ```
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- framework_lib/src/inputmodule.rs
+++ framework_lib/src/inputmodule.rs
@@ -16,7 +16,7 @@ pub fn check_inputmodule_version() -> Result<(), rusb::Error> {
         let vid = dev_descriptor.vendor_id();
         let pid = dev_descriptor.product_id();
         if vid != FRAMEWORK_VID || !FRAMEWORK16_INPUTMODULE_PIDS.contains(&pid) {
-            debug!("Skipping {:04X}:{:04X}", vid, pid);
+            trace!("Skipping {:04X}:{:04X}", vid, pid);
             continue;
         }
 
@@ -34,6 +34,29 @@ pub fn check_inputmodule_version() -> Result<(), rusb::Error> {
             println!("{}", i_product.unwrap_or_default());
         }
         println!("  Firmware Version: {}", dev_descriptor.device_version());
+
+        debug!("Address:      {:?}", dev.address());
+        debug!("Bus  Number:  {:?}", dev.bus_number());
+        debug!("Port Number:  {:?}", dev.port_number());
+        debug!("Port Numbers: {:?}", dev.port_numbers());
+        let port_numbers = dev.port_numbers();
+        let location = if let Ok(port_numbers) = port_numbers {
+            if port_numbers.len() == 2 {
+                match (port_numbers[0], port_numbers[1]) {
+                    (4, 2) => "[X] [ ] [ ]       [ ] [ ]",
+                    (4, 3) => "[ ] [X] [ ]       [ ] [ ]",
+                    (3, 1) => "[ ] [ ] [X]       [ ] [ ]",
+                    (3, 2) => "[ ] [ ] [ ]       [X] [ ]",
+                    (3, 3) => "[ ] [ ] [ ]       [ ] [X]",
+                    _ => "Unknown",
+                }
+            } else {
+                "Unknown"
+            }
+        } else {
+            "Unknown"
+        };
+        println!("  Location: {}", location);
     }
     Ok(())
 }
