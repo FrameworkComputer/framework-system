FrameworkComputer/framework-system

commit 49eb0b914ee2f11124614ba6d3f3259adf199fac

--inputdeck: Positive wording
Normal case should use all-positive wording.
If everything's good it should say "true" and "present"

Signed-off-by: Daniel Schaefer <dhs@frame.work>

framework_lib/src/chromium_ec/mod.rs
@@ -483,7 +483,7 @@ impl CrosEc {
         let is_present = |p| if p { "Present" } else { "Missing" };
 
         println!("Input Deck");
-        println!("  Chassis Open:        {}", intrusion.currently_open);
+        println!("  Chassis Closed:      {}", intrusion.currently_open);
         println!("  Power Button Board:  {}", is_present(pwrbtn.is_some()));
         println!("  Audio Daughterboard: {}", is_present(audio.is_some()));
         println!("  Touchpad:            {}", is_present(tp.is_some()));
@@ -511,7 +511,7 @@ impl CrosEc {
         let is_present = |p| if p { "Present" } else { "Missing" };
 
         println!("Input Deck");
-        println!("  Chassis Open:        {}", intrusion.currently_open);
+        println!("  Chassis Closed:      {}", intrusion.currently_open);
         println!("  Audio Daughterboard: {}", is_present(audio.is_some()));
         println!("  Touchpad:            {}", is_present(tp.is_some()));
 
@@ -521,7 +521,7 @@ impl CrosEc {
     pub fn print_fw16_inputdeck_status(&self) -> EcResult<()> {
         let intrusion = self.get_intrusion_status()?;
         let status = self.get_input_deck_status()?;
-        println!("Chassis Open:     {}", intrusion.currently_open);
+        println!("Chassis Closed:   {}", intrusion.currently_open);
         println!("Input Deck State: {:?}", status.state);
         println!("Touchpad present: {}", status.touchpad_present);
         println!("Positions:");

commit 6d7afb404d2e79b626b7be162446d4f9c5938e4a

--inputdeck: Fix Chassis Closed status
Fixes: 49eb0b914ee2f11124614ba6d3f3259adf199fac
That commit reversed the wording but not the variable.

Signed-off-by: Daniel Schaefer <dhs@frame.work>

framework_lib/src/chromium_ec/mod.rs
@@ -510,7 +510,7 @@ impl CrosEc {
         let is_present = |p| if p { "Present" } else { "Missing" };
 
         println!("Input Deck");
-        println!("  Chassis Closed:      {}", intrusion.currently_open);
+        println!("  Chassis Closed:      {}", !intrusion.currently_open);
         println!("  Power Button Board:  {}", is_present(pwrbtn.is_some()));
         println!("  Audio Daughterboard: {}", is_present(audio.is_some()));
         println!("  Touchpad:            {}", is_present(tp.is_some()));
@@ -538,7 +538,7 @@ impl CrosEc {
         let is_present = |p| if p { "Present" } else { "Missing" };
 
         println!("Input Deck");
-        println!("  Chassis Closed:      {}", intrusion.currently_open);
+        println!("  Chassis Closed:      {}", !intrusion.currently_open);
         println!("  Audio Daughterboard: {}", is_present(audio.is_some()));
         println!("  Touchpad:            {}", is_present(tp.is_some()));
 
@@ -548,7 +548,7 @@ impl CrosEc {
     pub fn print_fw16_inputdeck_status(&self) -> EcResult<()> {
         let intrusion = self.get_intrusion_status()?;
         let status = self.get_input_deck_status()?;
-        println!("Chassis Closed:   {}", intrusion.currently_open);
+        println!("Chassis Closed:   {}", !intrusion.currently_open);
         println!("Input Deck State: {:?}", status.state);
         println!("Touchpad present: {}", status.touchpad_present);
         println!("Positions:");