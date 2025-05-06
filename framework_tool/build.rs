7336362ac1b22f945226168ad903c83bee279b82
Daniel Schaefer <dhs@frame.work>
2025-05-06 05:47:04 -0700

    windows: Embed resource to always launch as admin
    Only in release builds, because running `cargo run` from non-admin
    terminal won't work with the resources.
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- framework_tool/build.rs
+++ framework_tool/build.rs
@@ -1,3 +1,11 @@
 fn main() {
-    static_vcruntime::metabuild();
+    if !cfg!(debug_assertions) {
+        // Statically link vcruntime to allow running on clean install
+        static_vcruntime::metabuild();
+
+        // Embed resources file to force running as admin
+        embed_resource::compile("framework_tool-manifest.rc", embed_resource::NONE)
+            .manifest_optional()
+            .unwrap();
+    }
 }
