1b64d90a3474e0bb0ecea9a3623b18b792f39739
Daniel Schaefer <dhs@frame.work>
2025-07-01 10:43:15 +0800

    uefi: Fix shell args when run from a script
    The UEFI tool wouldn't get any arugments when run from a UEFI shell
    script. Manually runnning from the shell would work, but not from e.g.
    startup.nsh
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- framework_uefi/src/main.rs
+++ framework_uefi/src/main.rs
@@ -12,11 +12,11 @@ extern crate alloc;
 use framework_lib::commandline;
 
 #[entry]
-fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+fn main(image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
     let bs = system_table.boot_services();
 
-    let args = commandline::uefi::get_args(bs);
+    let args = commandline::uefi::get_args(bs, image_handle);
     let args = commandline::parse(&args);
     if commandline::run_with_args(&args, false) == 0 {
         return Status::SUCCESS;
