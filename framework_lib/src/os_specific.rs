9a282baccfda5c49b0e1cc2ac180bbde63af5d07
Daniel Schaefer <dhs@frame.work>
2025-07-08 19:14:16 +0800

    try to set variable from windows
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- framework_lib/src/os_specific.rs
+++ framework_lib/src/os_specific.rs
@@ -3,6 +3,9 @@
 #[cfg(not(feature = "uefi"))]
 use std::{thread, time};
 
+#[cfg(windows)]
+use windows::{core::*, Win32::System::WindowsProgramming::*};
+
 /// Sleep a number of microseconds
 pub fn sleep(micros: u64) {
     #[cfg(not(feature = "uefi"))]
@@ -17,5 +20,25 @@ pub fn sleep(micros: u64) {
         let st = unsafe { uefi_services::system_table().as_ref() };
         let bs = st.boot_services();
         bs.stall(micros as usize);
+    }
+}
+
+#[cfg(windows)]
+pub fn set_dbx() -> Option<()> {
+    set_uefi_var("dbx", "d719b2cb-3d3a-4596-a3bc-dad00e67656f", &[], 0)
+}
+
+#[cfg(windows)]
+pub fn set_uefi_var(name: &str, guid: &str, value: &[u8], attributes: u32) -> Option<()> {
+    unsafe {
+        SetFirmwareEnvironmentVariableExW(
+            // PCWSTR
+            &HSTRING::from(name),
+            // PCWSTR
+            &HSTRING::from(guid),
+            Some(value.as_ptr() as *const core::ffi::c_void),
+            value.len() as u32,
+            attributes,
+        ).ok()
     }
 }
