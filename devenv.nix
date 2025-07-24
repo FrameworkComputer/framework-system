fd71dac89899a3ca5700e084008341da3a2a1744
Daniel Schaefer <dhs@frame.work>
2025-07-24 21:57:33 +0800

    devenv: Add dependencies to build and run uefi tool in QEMU
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- devenv.nix
+++ devenv.nix
@@ -3,9 +3,16 @@
 {
   packages = with pkgs; [
     systemd # libudev
+    # For UEFI building and testing
+    parted
+    gnumake
+    qemu
   ];
 
-  languages.rust.enable = true;
-  # https://devenv.sh/reference/options/#languagesrustchannel
-  languages.rust.channel = "stable";
+  languages.rust = {
+    enable = true;
+    targets = [ "x86_64-unknown-uefi" ];
+    # https://devenv.sh/reference/options/#languagesrustchannel
+    channel = "stable";
+  };
 }
