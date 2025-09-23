493e9f932651344b86f0a2bb7f7eae5a222689d4
Daniel Schaefer <dhs@frame.work>
2025-09-23 15:57:46 +0800

    Document SSD Expansion Card firmware/flash types
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

---
+++ framework_lib/src/ssd_expansion_card.rs
@@ -0,0 +1,7 @@
+/// Firmware Versions
+/// | Flash Die    | Firmware Version |
+/// |--------------|------------------|
+/// | Hynix V6     | UHFM00.x         |
+/// | Micron N28   | UHFM10.x         |
+/// | Micron N48   | UHFM30.x         |
+/// | Kioxia BiSC6 | UHFM90.x         |
