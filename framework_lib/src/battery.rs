e4eec2ecd1f74b478623f4ea5d2f261dc76e8e3e
Daniel Schaefer <dhs@frame.work>
2025-12-04 01:43:10 +0800

    battery: Decode more normal and manufacturing commands
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- framework_lib/src/battery.rs
+++ framework_lib/src/battery.rs
@@ -5,12 +5,12 @@ use alloc::vec::Vec;
 
 use crate::chromium_ec::i2c_passthrough::*;
 use crate::chromium_ec::{CrosEc, EcResult};
-//use crate::os_specific;
 
 #[repr(u16)]
 enum SmartBatReg {
     Mode = 0x03,
     Temp = 0x08,
+    Voltage = 0x09,
     ManufactureDate = 0x1B,
     SerialNum = 0x1C,
     CycleCount = 0x17,
@@ -18,6 +18,10 @@ enum SmartBatReg {
     ManufacturerName = 0x20,
     DeviceName = 0x21,
     Soh = 0x4F,
+    CellVoltage1 = 0x3C,
+    CellVoltage2 = 0x3D,
+    CellVoltage3 = 0x3E,
+    CellVoltage4 = 0x3F,
 }
 
 #[repr(u16)]
@@ -33,6 +37,9 @@ enum ManufReg {
     LifeTimeDataBlock1 = 0x60,
     LifeTimeDataBlock2 = 0x61,
     LifeTimeDataBlock3 = 0x62,
+    LifeTimeDataBlock4 = 0x63,
+    LifeTimeDataBlock5 = 0x64,
+    Soh = 0x77,
 }
 
 pub struct SmartBattery {
@@ -51,21 +58,33 @@ impl SmartBattery {
         }
     }
 
-    fn read_bytes(&self, ec: &CrosEc, addr: u16, len: u16) -> EcResult<Vec<u8>> {
-        let i2c_response = i2c_read(ec, self.i2c_port, self.i2c_addr >> 1, addr, len)?;
-        i2c_response.is_successful()?;
-        Ok(i2c_response.data)
+    fn unseal(&self, ec: &CrosEc, key1: u16, key2: u16) -> EcResult<()> {
+        i2c_write_block(ec, self.i2c_port, self.i2c_addr >> 1, 0x00, &key1.to_le_bytes())?;
+        i2c_write_block(ec, self.i2c_port, self.i2c_addr >> 1, 0x00, &key2.to_le_bytes())?;
+        Ok(())
     }
-    fn read_i16(&self, ec: &CrosEc, addr: u16) -> EcResult<i16> {
+
+    fn seal(&self, ec: &CrosEc) -> EcResult<()> {
+        i2c_write(ec, self.i2c_port, self.i2c_addr >> 1, 0x00, &[0x30, 0x00])?;
+        Ok(())
+    }
+
+    fn read_bytes(&self, ec: &CrosEc, addr: u16, len: u16) -> EcResult<Vec<u8>> {
+        let i2c_response = i2c_read(ec, self.i2c_port, self.i2c_addr >> 1, addr, len + 1)?;
+        i2c_response.is_successful()?;
+        debug_assert_eq!(i2c_response.data[0], len as u8);
+        Ok(i2c_response.data[1..].to_vec())
+    }
+    fn read_i16(&self, ec: &CrosEc, addr: u16) -> EcResult<u16> {
         let i2c_response = i2c_read(ec, self.i2c_port, self.i2c_addr >> 1, addr, 0x02)?;
         i2c_response.is_successful()?;
-        Ok(i16::from_le_bytes([
-            i2c_response.data[1],
+        Ok(u16::from_le_bytes([
+            i2c_response.data[0],
             i2c_response.data[1],
         ]))
     }
     fn read_string(&self, ec: &CrosEc, addr: u16) -> EcResult<String> {
-        let i2c_response = i2c_read(ec, self.i2c_port, self.i2c_addr >> 1, addr, 16)?;
+        let i2c_response = i2c_read(ec, self.i2c_port, self.i2c_addr >> 1, addr, 32)?;
         i2c_response.is_successful()?;
 
         // First byte is the returned string length
@@ -87,9 +106,17 @@ impl SmartBattery {
             "Manuf Date:    {:04X?}",
             self.read_i16(ec, SmartBatReg::ManufactureDate as u16)?
         );
+        let temp = self.read_i16(ec, SmartBatReg::Temp as u16)?;
         println!(
-            "Temperature:   {:?}",
-            self.read_i16(ec, SmartBatReg::Temp as u16)?
+            "Temperature:   {}.{}C",
+            temp / 100,
+            temp % 100
+        );
+        let voltage = self.read_i16(ec, SmartBatReg::Voltage as u16)?;
+        println!(
+            "Voltage:       {}.{}V",
+            voltage / 1000,
+            voltage % 1000
         );
         println!(
             "Cycle Count:   {:?}",
@@ -103,13 +130,19 @@ impl SmartBattery {
             "Manuf Name:    {}",
             self.read_string(ec, SmartBatReg::ManufacturerName as u16)?
         );
-        println!(
-            "StateOfHealth: {:?}",
-            self.read_i16(ec, SmartBatReg::Soh as u16)?
-        );
+
+        // Default key - does not work on our battery, it's changed during manufacturing!
+        self.unseal(ec, 0x0414, 0x3672).unwrap();
 
         // Need to unseal for access
         // SE [US] [FA]
+        let soh = self.read_bytes(ec, ManufReg::Soh as u16, 4)?;
+        println!(
+            "StateOfHealth: {}mAh, {}.{}Wh",
+            u16::from_le_bytes([soh[0], soh[1]]),
+            u16::from_le_bytes([soh[2], soh[3]]) / 100,
+            u16::from_le_bytes([soh[2], soh[3]]) % 100,
+        );
         println!(
             "Safety Alert:  {:?}",
             self.read_i16(&ec, ManufReg::SafetyAlert as u16)?
@@ -126,21 +159,118 @@ impl SmartBattery {
             "PFStatus:      {:?}",
             self.read_i16(&ec, ManufReg::SafetyAlert as u16)?
         );
+        let lifetime1 = self.read_bytes(ec, ManufReg::LifeTimeDataBlock1 as u16, 32)?;
+        println!("LifeTime1");
         println!(
-            "LifeTime1      {:X?}",
-            self.read_bytes(ec, ManufReg::LifeTimeDataBlock1 as u16, 4)?
+            "  Cell 1 Max Voltage: {}mV",
+            u16::from_le_bytes([lifetime1[0], lifetime1[1]])
         );
         println!(
-            "LifeTime2      {:X?}",
-            self.read_bytes(ec, ManufReg::LifeTimeDataBlock2 as u16, 4)?
+            "         Min Voltage: {}mV",
+            u16::from_le_bytes([lifetime1[8], lifetime1[9]])
+        );
+        println!(
+            "  Cell 2 Max Voltage: {}mV",
+            u16::from_le_bytes([lifetime1[2], lifetime1[3]])
+        );
+        println!(
+            "         Min Voltage: {}mV",
+            u16::from_le_bytes([lifetime1[10], lifetime1[11]])
+        );
+        println!(
+            "  Cell 3 Max Voltage: {}mV",
+            u16::from_le_bytes([lifetime1[4], lifetime1[5]])
+        );
+        println!(
+            "         Min Voltage: {}mV",
+            u16::from_le_bytes([lifetime1[12], lifetime1[13]])
+        );
+        println!(
+            "  Cell 4 Max Voltage: {}mV",
+            u16::from_le_bytes([lifetime1[6], lifetime1[7]])
+        );
+        println!(
+            "         Min Voltage: {}mV",
+            u16::from_le_bytes([lifetime1[14], lifetime1[15]])
+        );
+        println!(
+            "  Max Delta Cell Voltage: {}mV",
+            u16::from_le_bytes([lifetime1[16], lifetime1[17]])
+        );
+        println!(
+            "  Max Charge Current:     {}mA",
+            u16::from_le_bytes([lifetime1[18], lifetime1[19]])
+        );
+        println!(
+            "  Max Discharge Current:  {}mA",
+            u16::from_le_bytes([lifetime1[20], lifetime1[21]])
+        );
+        println!(
+            "  Max Avg Dsg Current:    {}mA",
+            u16::from_le_bytes([lifetime1[22], lifetime1[23]])
+        );
+        println!(
+            "  Max Avg Dsg Power:      {}mW",
+            u16::from_le_bytes([lifetime1[24], lifetime1[25]])
+        );
+        println!(
+            "  Max Temp Cell:          {}C",
+            lifetime1[27]
+        );
+        println!(
+            "  Min Temp Cell:          {}C",
+            lifetime1[28]
+        );
+        println!(
+            "  Max Delta Cell temp:    {}K",
+            lifetime1[29]
+        );
+        println!(
+            "  Max Temp Int Sensor:    {}C",
+            lifetime1[29]
+        );
+        println!(
+            "  Min Temp Int Sensor:    {}C",
+            lifetime1[30]
+        );
+        println!(
+            "  Max Temp Fet:           {}C",
+            lifetime1[31]
+        );
+        let lifetime2 = self.read_bytes(ec, ManufReg::LifeTimeDataBlock2 as u16, 20)?; // 8?
+        println!("LifeTime2");
+        println!("  No. of Shutdowns:       {}
+  No. of Partial Resets:  {}
+  No. of Full Resets:     {}
+  No. of WDT resets:      {}
+  CB Time Cell 1:         {}
+  CB Time Cell 2:         {}
+  CB Time Cell 3:         {}
+  CB Time Cell 4:         {}",
+            lifetime2[0],
+            lifetime2[1],
+            lifetime2[2],
+            lifetime2[3],
+            lifetime2[4],
+            lifetime2[5],
+            lifetime2[6],
+            lifetime2[7],
         );
         println!(
             "LifeTime3      {:X?}",
             self.read_bytes(ec, ManufReg::LifeTimeDataBlock3 as u16, 4)?
         );
+        println!(
+            "LifeTime4      {:X?}",
+            self.read_bytes(ec, ManufReg::LifeTimeDataBlock4 as u16, 32)?
+        );
+        println!(
+            "LifeTime5      {:X?}",
+            self.read_bytes(ec, ManufReg::LifeTimeDataBlock5 as u16, 32)?
+        );
 
-        // i2c_write(ec, i2c_port, (i2c_addr + 2) >> 1, 0x70, &[0x00])?;
-        // os_specific::sleep(50_000);
+        // Seal back again after we're finished
+        self.seal(ec).unwrap();
 
         Ok(())
     }
