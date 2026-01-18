ff6f1246f6a83be71b6c8d740e77ebe1d8cabbce
Daniel Schaefer <dhs@frame.work>
2026-01-18 16:17:57 +0800

    simplify fs by using uefi-rs definitions
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- framework_lib/src/fw_uefi/fs.rs
+++ framework_lib/src/fw_uefi/fs.rs
@@ -1,149 +1,94 @@
 use alloc::vec;
 use alloc::vec::Vec;
-use uefi::prelude::*;
-use uefi::proto::shell::Shell;
-use uefi_raw::protocol::shell::ShellProtocol;
-//use uefi::proto::shell::FileOpenMode;
-use core::ffi::c_void;
 use core::mem::MaybeUninit;
-use core::ptr::NonNull;
-use uefi::Result;
+use uefi::boot;
+use uefi::proto::shell::Shell;
+use uefi::{CString16, Result, Status, StatusExt};
+use uefi_raw::protocol::file_system::FileMode;
+use uefi_raw::protocol::shell::ShellProtocol;
+use uefi_raw::protocol::shell_params::ShellFileHandle;
 
-#[repr(transparent)]
-#[derive(Clone, Copy, Debug)]
-pub struct ShellFileHandle(NonNull<c_void>);
-
-const FILE_MODE_READ: u64 = 0x0000000000000001;
-const FILE_MODE_WRITE: u64 = 0x0000000000000002;
-const FILE_MODE_CREATE: u64 = 0x8000000000000000;
-
-pub fn wstr(string: &str) -> Vec<u16> {
-    let mut wstring = vec![];
-
-    for c in string.chars() {
-        wstring.push(c as u16);
+fn get_shell_protocol() -> &'static ShellProtocol {
+    let handle = boot::get_handle_for_protocol::<Shell>().expect("No Shell handles");
+    let shell =
+        boot::open_protocol_exclusive::<Shell>(handle).expect("Failed to open Shell protocol");
+    // SAFETY: The Shell wrapper contains the raw ShellProtocol
+    unsafe {
+        let proto: &ShellProtocol = core::mem::transmute(shell.get().unwrap());
+        // Leak to get 'static lifetime - protocol stays valid while shell is running
+        core::mem::forget(shell);
+        proto
     }
-    wstring.push(0);
-
-    wstring
 }
 
 pub fn shell_read_file(path: &str) -> Option<Vec<u8>> {
-    let handle = boot::get_handle_for_protocol::<Shell>().expect("No Shell handles");
-    let mut shell =
-        boot::open_protocol_exclusive::<Shell>(handle).expect("Failed to open Shell protocol");
-    let shell = unsafe {
-        let proto: &ShellProtocol = std::mem::transmute(shell.get().unwrap());
-        proto
-    };
-
-    println!("Opened shell protocol");
-
-    debug_assert_eq!(shell.major_version, 2);
-    debug_assert_eq!(shell.minor_version, 2);
-
-    println!(
-        "Shell protocol ver: {}.{}",
-        shell.major_version, shell.minor_version
-    );
+    let shell = get_shell_protocol();
+    let c_path = CString16::try_from(path).ok()?;
 
     unsafe {
-        let c_path = wstr(path);
-        let mut mode = FILE_MODE_READ;
-        let mut handle: MaybeUninit<*const c_void> = MaybeUninit::zeroed();
-        (shell.open_file_by_name)(c_path.as_ptr(), handle.as_mut_ptr().cast(), mode);
-
-        println!("Opened file");
+        let mut handle: MaybeUninit<ShellFileHandle> = MaybeUninit::zeroed();
+        let status = (shell.open_file_by_name)(
+            c_path.as_ptr().cast(),
+            handle.as_mut_ptr(),
+            FileMode::READ.bits(),
+        );
+        if status.is_error() {
+            return None;
+        }
 
         let file_handle = handle.assume_init();
 
-        let mut file_size = 0;
-        println!("get_file_size");
-        let res = (shell.get_file_size)(file_handle, &mut file_size);
-        // let file_size = res.unwrap();
+        let mut file_size: u64 = 0;
+        let status = (shell.get_file_size)(file_handle, &mut file_size);
+        if status.is_error() {
+            let _ = (shell.close_file)(file_handle);
+            return None;
+        }
 
         let mut buffer: Vec<u8> = vec![0; file_size as usize];
         let mut read_size = file_size as usize;
-        println!("read_file {} bytes", file_size);
-        (shell.read_file)(
+        let status = (shell.read_file)(
             file_handle,
             &mut read_size,
-            buffer.as_mut_ptr() as *mut c_void,
+            buffer.as_mut_ptr().cast(),
         );
 
-        println!("close_file");
+        let _ = (shell.close_file)(file_handle);
 
-        //  TODO: Make it auto-close using Rust destructors
-        (shell.close_file)(file_handle);
+        if status.is_error() {
+            return None;
+        }
 
-        println!("Done");
-
+        buffer.truncate(read_size);
         Some(buffer)
     }
 }
 
 pub fn shell_write_file(path: &str, data: &[u8]) -> Result {
-    let handle = boot::get_handle_for_protocol::<Shell>().expect("No Shell handles");
-    let mut shell =
-        boot::open_protocol_exclusive::<Shell>(handle).expect("Failed to open Shell protocol");
-    let shell = unsafe {
-        let proto: &ShellProtocol = std::mem::transmute(shell.get().unwrap());
-        proto
-    };
-
-    debug_assert_eq!(shell.major_version, 2);
-    debug_assert_eq!(shell.minor_version, 2);
+    let shell = get_shell_protocol();
+    let c_path = CString16::try_from(path).map_err(|_| uefi::Error::from(Status::INVALID_PARAMETER))?;
 
     unsafe {
-        // let mode = FileOpenMode::Read as u64 + FileOpenMode::Write as u64 + FileOpenMode::Create as u64;
-        let mode = FILE_MODE_READ + FILE_MODE_WRITE + FILE_MODE_CREATE;
-        let c_path = wstr(path);
-        let mut handle: MaybeUninit<*const c_void> = MaybeUninit::zeroed();
-        (shell.open_file_by_name)(c_path.as_ptr(), handle.as_mut_ptr().cast(), mode);
+        let mode = FileMode::READ | FileMode::WRITE | FileMode::CREATE;
+        let mut handle: MaybeUninit<ShellFileHandle> = MaybeUninit::zeroed();
+        (shell.open_file_by_name)(
+            c_path.as_ptr().cast(),
+            handle.as_mut_ptr(),
+            mode.bits(),
+        )
+        .to_result()?;
+
         let file_handle = handle.assume_init();
 
-        //// TODO: Free file_info buffer
-        //let file_info = (shell.0.GetFileInfo)(file_handle);
-        //if file_info.is_null() {
-        //    println!("Failed to get file info");
-        //    return ret;
-        //}
+        let mut write_size = data.len();
+        let status = (shell.write_file)(
+            file_handle,
+            &mut write_size,
+            data.as_ptr() as *mut _,
+        );
 
-        //// Not sure if it's useful to set FileInfo
-        ////let mut file_info = unsafe {
-        ////    &mut *(file_info as *mut FileInfo)
-        ////};
-        ////println!("file_info.Size: {}", file_info.Size);
+        let _ = (shell.close_file)(file_handle);
 
-        ////if file_info.Size != 0 {
-        ////    file_info.Size = 0;
-        ////    let ret = (shell.0.SetFileInfo)(file_handle, file_info);
-        ////    if ret.0 != 0 {
-        ////        println!("Failed to set file info");
-        ////        return ret;
-        ////    }
-        ////}
-
-        //let mut buffer_size = data.len() as usize;
-        //let ret = (shell.0.WriteFile)(file_handle, &mut buffer_size, data.as_ptr());
-        //if ret.0 != 0 {
-        //    println!("Failed to write file");
-        //    return ret;
-        //}
-        //if buffer_size != data.len() {
-        //    println!(
-        //        "Failed to write whole buffer. Instead of {} wrote {} bytes.",
-        //        data.len(),
-        //        buffer_size
-        //    );
-        //    return Status(1);
-        //}
-
-        let mut read_size = data.len();
-        (shell.write_file)(file_handle, &mut read_size, data.as_ptr() as *mut c_void);
-
-        (shell.close_file)(file_handle);
-
-        Status::SUCCESS.to_result()
+        status.to_result()
     }
 }
