b7b2e46fcb093881ed57cad786399a5c511c06a7
Daniel Schaefer <dhs@frame.work>
2026-01-26 14:11:00 +0800

    cargo fmt
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- framework_lib/src/fw_uefi/fs.rs
+++ framework_lib/src/fw_uefi/fs.rs
@@ -59,11 +59,7 @@ pub fn shell_read_file(path: &str) -> Option<Vec<u8>> {
 
         let mut buffer: Vec<u8> = vec![0; file_size as usize];
         let mut read_size = file_size as usize;
-        let status = (shell.read_file)(
-            file_handle,
-            &mut read_size,
-            buffer.as_mut_ptr().cast(),
-        );
+        let status = (shell.read_file)(file_handle, &mut read_size, buffer.as_mut_ptr().cast());
 
         let _ = (shell.close_file)(file_handle);
 
@@ -78,26 +74,19 @@ pub fn shell_read_file(path: &str) -> Option<Vec<u8>> {
 
 pub fn shell_write_file(path: &str, data: &[u8]) -> Result {
     let shell = get_shell_protocol();
-    let c_path = CString16::try_from(path).map_err(|_| uefi::Error::from(Status::INVALID_PARAMETER))?;
+    let c_path =
+        CString16::try_from(path).map_err(|_| uefi::Error::from(Status::INVALID_PARAMETER))?;
 
     unsafe {
         let mode = FileMode::READ | FileMode::WRITE | FileMode::CREATE;
         let mut handle: MaybeUninit<ShellFileHandle> = MaybeUninit::zeroed();
-        (shell.open_file_by_name)(
-            c_path.as_ptr().cast(),
-            handle.as_mut_ptr(),
-            mode.bits(),
-        )
-        .to_result()?;
+        (shell.open_file_by_name)(c_path.as_ptr().cast(), handle.as_mut_ptr(), mode.bits())
+            .to_result()?;
 
         let file_handle = handle.assume_init();
 
         let mut write_size = data.len();
-        let status = (shell.write_file)(
-            file_handle,
-            &mut write_size,
-            data.as_ptr() as *mut _,
-        );
+        let status = (shell.write_file)(file_handle, &mut write_size, data.as_ptr() as *mut _);
 
         let _ = (shell.close_file)(file_handle);
 
