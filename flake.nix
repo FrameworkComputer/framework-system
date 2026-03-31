6f2713df681da5682f64e7c5391b15be72eda3dc
Daniel Schaefer <dhs@frame.work>
2026-03-31 14:43:35 +0800

    bump to 0.6.2
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- flake.nix
+++ flake.nix
@@ -91,7 +91,7 @@
           in
           rustPlatform.buildRustPackage {
             pname = "framework_tool";
-            version = "0.6.1";
+            version = "0.6.2";
 
             src = buildSrc;
 
@@ -144,7 +144,7 @@
           in
           rustPlatformWindows.buildRustPackage {
             pname = "framework_tool";
-            version = "0.6.1";
+            version = "0.6.2";
 
             src = buildSrc;
 
@@ -194,7 +194,7 @@
           in
           rustPlatform.buildRustPackage {
             pname = "framework_uefi";
-            version = "0.6.1";
+            version = "0.6.2";
 
             src = buildSrc;
 
